a591391faa91b4f268d122349dbe4891a50230fe
Mitchell Gardepe <mitchellgardepe@Mitchells-MacBook-Pro.local>
2019-05-17 18:07:35 -0400

    Learning Angular is fun

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+
+import { UserComponent } from "./components/user/user.component";
+import { DataService } from "./services/data.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, UserComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
